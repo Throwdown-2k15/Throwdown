409946291a8e0eeec19dffdbf6877a6028ab249b
Michael McGrath & Sulu Fiti <pair+michaelmcgrath.sulufiti@devbootcamp.com>
2015-10-15 10:57:05 +1300

    Add dashboard and edit page

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,4 +1,10 @@
 class SiteController < ApplicationController
   def index
   end
+
+  def dashboard
+  end
+
+  def edit
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root 'site#index'
+  get '/dashboard' => 'site#dashboard'
+  get '/edit' => 'site#edit'
 end
